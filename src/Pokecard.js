90f831a7f88a1f304e8ae2eff5fc0f214ec9cee9
Jeremy Adams <Beoron@gmail.com>
2019-06-19 16:31:20 -0500

    Style Pokecard and Pokedex

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -6,11 +6,11 @@ class Pokecard extends Component {
     render() {
         let imgSrc = `${POKE_API}${this.props.id}.png`;
         return (
-            <div className="Pokecard">
-                <h1>{this.props.name}</h1>
+            <div className = "Pokecard">
+                <h1 className = "Pokecard-title">{this.props.name}</h1>
                 <img src={imgSrc} alt={this.props.name} />
-                <div>Type: {this.props.type}</div>
-                <div>EXP: {this.props.exp}</div>
+                <div className = "Pokecard-data">Type: {this.props.type}</div>
+                <div className = "Pokecard-data">EXP: {this.props.exp}</div>
             </div>
         );
     }
